1c8cc7c674c8193deeba78869f4bb9bb6e709907
t3rmin41 <t33rmin41@gmail.com>
2018-06-26 15:12:42 +0300

    Difference between locking outside and inside run() method

--- src/test/java/com/concurrency/project/test/sample/UnsafeSequenceReentrantLockInsideRunnableTest.java
+++ src/test/java/com/concurrency/project/test/sample/UnsafeSequenceReentrantLockInsideRunnableTest.java
@@ -8,43 +8,45 @@ import org.junit.Test;
 import com.concurrency.project.sample.UnsafeSequence;
 import com.concurrency.project.tester.MultithreadedStressTester;
 
-public class UnsafeSequenceLockTest {
+public class UnsafeSequenceReentrantLockInsideRunnableTest {
 
   private final ReentrantLock lock = new ReentrantLock();
   
-  private static final int THREAD_COUNT = 100;
+  private static final int THREAD_COUNT = 500;
   private static final int INVOCATION_COUNT = 1;
   
   private UnsafeSequence seq = null;
-  private MultithreadedStressTester stressTester = null;
+  private MultithreadedStressTester stressTester1 = null;
+  private MultithreadedStressTester stressTester2 = null;
   
   @Before
   public void before() {
     seq =  new UnsafeSequence();
-    stressTester = new MultithreadedStressTester(THREAD_COUNT, INVOCATION_COUNT);
+    stressTester1 = new MultithreadedStressTester(THREAD_COUNT, INVOCATION_COUNT);
+    stressTester2 = new MultithreadedStressTester(THREAD_COUNT, INVOCATION_COUNT);
   }
   
+
   @Test
-  public void testSequence() throws InterruptedException {
+  public void testSequenceInsideRunnableLock() throws InterruptedException {
     //race condition occurs when multiple threads might read (hence, generate) the same value when in fact 
     //have to read and generate different value each read operation
-    this.lock.lock();
-    try {
-      stressTester.stress(new Runnable() {
+      stressTester2.stress(new Runnable() {
         public void run() {
-          seq.increase();
+          lock.lock();
+          try {
+            seq.increase();
+          } finally {
+            lock.unlock();
+          }
         }
       });
-    } finally {
-      this.lock.unlock();
-    }
-    stressTester.shutdown();
+    stressTester2.shutdown();
   }
   
   @After
   public void testCount() {
-    assertEquals("100 Threads running increase() in parallel should lead to 100" , 100 , seq.getCount());
+    assertEquals("500 Threads running increase() in parallel should lead to 500" , 500 , seq.getCount());
   }
-  
-  
+
 }
---
+++ src/test/java/com/concurrency/project/test/sample/UnsafeSequenceReentrantLockOutsideRunnableTest.java
@@ -0,0 +1,52 @@
+package com.concurrency.project.test.sample;
+
+import static org.junit.Assert.assertEquals;
+import java.util.concurrent.locks.ReentrantLock;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import com.concurrency.project.sample.UnsafeSequence;
+import com.concurrency.project.tester.MultithreadedStressTester;
+
+public class UnsafeSequenceReentrantLockOutsideRunnableTest {
+
+  private final ReentrantLock lock = new ReentrantLock();
+  
+  private static final int THREAD_COUNT = 500;
+  private static final int INVOCATION_COUNT = 1;
+  
+  private UnsafeSequence seq = null;
+  private MultithreadedStressTester stressTester1 = null;
+  private MultithreadedStressTester stressTester2 = null;
+  
+  @Before
+  public void before() {
+    seq =  new UnsafeSequence();
+    stressTester1 = new MultithreadedStressTester(THREAD_COUNT, INVOCATION_COUNT);
+    stressTester2 = new MultithreadedStressTester(THREAD_COUNT, INVOCATION_COUNT);
+  }
+
+  @Test
+  public void testSequenceOutsideRunnableLock() throws InterruptedException {
+    //race condition occurs when multiple threads might read (hence, generate) the same value when in fact 
+    //have to read and generate different value each read operation
+    this.lock.lock();
+    try {
+      stressTester1.stress(new Runnable() {
+        public void run() {
+            seq.increase();
+        }
+      });
+    } finally {
+      this.lock.unlock();
+    }
+    stressTester1.shutdown();
+  }
+  
+  
+  @After
+  public void testCount() {
+    assertEquals("500 Threads running increase() in parallel should lead to 500" , 500 , seq.getCount());
+  }
+
+}
